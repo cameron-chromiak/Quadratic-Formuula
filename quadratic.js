78a62e273453397d3fd7e9ab28aac456d280836c
cameron-chromiak <cam.chromiak@hotmail.com>
2018-10-18 13:35:47 -0400

    discrimnant works but needs denom and formatting and good stuff

--- quadratic.js
+++ quadratic.js
@@ -1,6 +1,24 @@
 
 document.getElementById('button').addEventListener("click", main)
+
+// calcDenom(a){
+//   num = 2*a;
+//   return num;
+// }
+
+function calcDisc(a, b, c){
+  var  num = eval((-b*-b) -4*a*c);
+  return num;
+}
 function main(){
-  var aValue = document.getElementsByName('aValue')[0];
-  alert(aValue);
+  var aValue = document.getElementById('a-Input').value;
+  var bValue = document.getElementById('b-Input').value;
+  var cValue = document.getElementById('c-Input').value;
+  //alert(aValue + bValue + cValue);
+  var discriminant, denom;
+
+  discriminant = calcDisc(aValue, bValue, cValue);
+  alert("n"+discriminant);
+  // denom = calcDenom(aValue);
+  // alert("d"+denom);
 }
